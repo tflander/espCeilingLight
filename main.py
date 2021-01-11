a7685e46b2183c49eb5bd82f3b3d75a3ca03787d
todd flanders <toddfbass@gmail.com>
2021-01-11 11:59:12 -0500

    add TouchButton class

--- main.py
+++ main.py
@@ -1,85 +1,27 @@
 import machine
-import math, time, utime
-import _thread
-from array import array
-import uasyncio
+import math, utime
 from touch_button import *
 
 from ntptime import settime
 
 settime()
 
-led = machine.PWM(machine.Pin(2), freq=60)
-
-touch_adjust_parameters = AdustParameters(
+touch_adjust_parameters = AdjustParameters(
     max_released = 600,
     min_released = 400,
     max_selected = 200,
     min_selected = 50
 )
 
-
-async def led_on(event):
-    await event.wait()
-    led.duty(1023)
-
-
-async def led_off(event):
-    await event.wait()
-    led.duty(0)
-
-
-async def toggle_led_with_uasyncio():
-
-    while True:
-        touch_selected_event = uasyncio.Event()
-        touch_release_event = uasyncio.Event()
-
-        led_on_task = uasyncio.create_task(led_on(touch_selected_event))
-        led_off_task = uasyncio.create_task(led_off(touch_release_event))
-
-        await uasyncio.sleep(1)
-        touch_selected_event.set()
-        await led_on_task
-
-        await uasyncio.sleep(1)
-        touch_release_event.set()
-        await led_off_task
-
-# uasyncio.run(toggle_led_with_uasyncio())
-
-
-def pulse(l, t):
-     for i in range(20):
-         l.duty(int(math.sin(i / 10 * math.pi) * 500 + 500))
-         time.sleep_ms(t)
-
-def timeSpike():
-    print(utime.mktime(utime.localtime()) - utime.mktime((2021, 1, 4, 17, 40, 4, 0, 4)))
-
-touch = machine.TouchPad(machine.Pin(4))
-
-def sample_touch():
-    samples = list()
-    for i in range(200):
-        samples.append(touch.read())
-        time.sleep_ms(20)
-
-    print("max = ", max(samples))
-    print("min = ", min(samples))
-    print("avg = ", sum(samples) / len(samples))
-
-def show_touch():
-    while True:
-        print(touch.read())
-        time.sleep_ms(200)
+led = machine.PWM(machine.Pin(2), freq=60)
+led_touch_sensor = TouchButton(machine.Pin(4), touch_adjust_parameters)
 
 
 def activate_led_by_touch_latched():
     current_state = TouchState.UNKNOWN
 
     while True:
-        new_state = wait_for_touch_state_change(current_state)
+        new_state = led_touch_sensor.wait_for_state_change(current_state)
         if new_state == TouchState.SELECTED:
             if led.duty() > 0:
                 led.duty(0)
@@ -92,7 +34,7 @@ def activate_led_by_touch_momentary():
     current_state = TouchState.UNKNOWN
 
     while True:
-        new_state = wait_for_touch_state_change(current_state)
+        new_state = led_touch_sensor.wait_for_state_change(current_state)
         if new_state == TouchState.SELECTED:
             led.duty(1023)
         elif new_state == TouchState.RELEASED:
@@ -100,27 +42,50 @@ def activate_led_by_touch_momentary():
         current_state = new_state
 
 
-def wait_for_touch_state_change(current_state):
-    while True:
-        s = touch.read()
-        if touch_adjust_parameters.min_released < s < touch_adjust_parameters.max_released:
-            new_state = TouchState.RELEASED
-        elif touch_adjust_parameters.min_selected < s < touch_adjust_parameters.max_selected:
-            new_state = TouchState.SELECTED
-        elif s < touch_adjust_parameters.min_selected or s > touch_adjust_parameters.max_released:
-            new_state = TouchState.OUT_OF_RANGE
-        else:
-            new_state = TouchState.DEAD_BAND
+async def led_on(event):
+    await event.wait()
+    led.duty(1023)
 
-        if new_state != current_state:
-            return new_state
-        time.sleep_ms(20)
+
+async def led_off(event):
+    await event.wait()
+    led.duty(0)
+
+
+# async def toggle_led_with_uasyncio():
+#
+#     while True:
+#         touch_selected_event = uasyncio.Event()
+#         touch_release_event = uasyncio.Event()
+#
+#         led_on_task = uasyncio.create_task(led_on(touch_selected_event))
+#         led_off_task = uasyncio.create_task(led_off(touch_release_event))
+#
+#         await uasyncio.sleep(1)
+#         touch_selected_event.set()
+#         await led_on_task
+#
+#         await uasyncio.sleep(1)
+#         touch_release_event.set()
+#         await led_off_task
+
+# uasyncio.run(toggle_led_with_uasyncio())
+
+
+def pulse(l, t):
+     for i in range(20):
+         l.duty(int(math.sin(i / 10 * math.pi) * 500 + 500))
+         utime.sleep_ms(t)
+
+
+def timeSpike():
+    print(utime.mktime(utime.localtime()) - utime.mktime((2021, 1, 4, 17, 40, 4, 0, 4)))
 
 
 def test_thread():
     while True:
         print("Hello from thread")
-        time.sleep(2)
+        utime.sleep(2)
 
 
 # _thread.start_new_thread(testThread, ())
--- touch_button.py
+++ touch_button.py
@@ -1,3 +1,6 @@
+import utime
+from machine import Pin, TouchPad
+
 class TouchState:
     UNKNOWN = 1
     RELEASED = 2
@@ -6,7 +9,7 @@ class TouchState:
     OUT_OF_RANGE = 5
 
 
-class AdustParameters:
+class AdjustParameters:
 
     def __init__(self, max_released, min_released, max_selected, min_selected):
         self.max_released = max_released
@@ -14,3 +17,25 @@ class AdustParameters:
         self.max_selected = max_selected
         self.min_selected = min_selected
 
+
+class TouchButton:
+
+    def __init__(self, touch_pin: Pin, adjust_parameters: AdjustParameters):
+        self.touch = TouchPad(touch_pin)
+        self.adjust_parameters = adjust_parameters
+
+    def wait_for_state_change(self, current_state):
+        while True:
+            s = self.touch.read()
+            if self.adjust_parameters.min_released < s < self.adjust_parameters.max_released:
+                new_state = TouchState.RELEASED
+            elif self.adjust_parameters.min_selected < s < self.adjust_parameters.max_selected:
+                new_state = TouchState.SELECTED
+            elif s < self.adjust_parameters.min_selected or s > self.adjust_parameters.max_released:
+                new_state = TouchState.OUT_OF_RANGE
+            else:
+                new_state = TouchState.DEAD_BAND
+
+            if new_state != current_state:
+                return new_state
+            utime.sleep_ms(20)
