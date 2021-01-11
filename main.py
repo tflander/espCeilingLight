c6ca8c79b09df4867c39fbc8847f64ae2a7e4324
todd flanders <toddfbass@gmail.com>
2021-01-10 19:29:04 -0500

    bug fix touch_state

--- main.py
+++ main.py
@@ -48,22 +48,22 @@ def touch_state():
     min_released = 400
     max_selected = 200
     min_selected = 50
-
+    counter = 0
     while True:
-        new_state = TouchState.UNKNOWN
         s = touch.read()
         if min_released < s < max_released:
             new_state = TouchState.RELEASED
         elif min_selected < s < max_selected:
             new_state = TouchState.SELECTED
-        elif s < min_selected or s > max_selected:
-            new_state = TouchState.DEAD_BAND
-        else:
+        elif s < min_selected or s > max_released:
             new_state = TouchState.OUT_OF_RANGE
+        else:
+            new_state = TouchState.DEAD_BAND
 
         if new_state != current_state:
+            counter += 1
             current_state = new_state
-            print(new_state, s)
+            print(new_state)
 
         time.sleep_ms(20)
 
@@ -74,52 +74,3 @@ def testThread():
 
 
 # _thread.start_new_thread(testThread, ())
-
-class RingBuffer:
-
-    def __init__(self, size):
-        self.size = size
-        self.buffer = array('I')
-        self.index = 0
-        for i in range(0,5):
-            self.buffer.append(0)
-
-    def update(self, n):
-        self.buffer[self.index] = n
-        self.index += 1
-        if self.index == self.size:
-            self.index = 0
-
-    def mean(self):
-        return sum(rb.buffer)/self.size
-
-    def std(self):
-        mu = self.mean()
-
-        def deviation_squared(n):
-            return (mu-n)**2
-
-        sum_of_squares = sum(map(deviation_squared, self.buffer))
-        return math.sqrt(sum_of_squares / self.size)
-
-    def z_scores(self):
-        mu = self.mean()
-        std = self.std()
-
-        def z_score(n):
-            return (n-mu)/std
-
-        def zero(n: object):
-            return 0
-
-        if std == 0:
-            return map(zero, self.buffer)
-
-        return map(z_score, self.buffer)
-
-    def is_stable(self):
-        scores = list(self.z_scores())
-        return max(scores) < 1.0 and min(scores) > -1.0
-
-
-rb = RingBuffer(5)
