73836fe921bf8d28cbdc772db4036fbc938c06ef
todd flanders <toddfbass@gmail.com>
2021-01-12 17:05:33 -0500

    create library of lighting modes

---
+++ lighting_modes.py
@@ -0,0 +1,66 @@
+class AbstractLightingMode:
+
+    current_mode_index = 0
+
+    def next(self):
+        self.current_mode_index += 1
+        if self.current_mode_index == len(self.modes):
+            self.current_mode_index = 0
+        return self.current_mode()
+
+    def current_mode(self):
+        return self.modes[self.current_mode_index]
+
+
+class WhiteModes(AbstractLightingMode):
+    FULL = 1023
+    HALF = 512
+    QUARTER = 256
+
+    modes = (FULL, HALF, QUARTER)
+
+    def activate(self):
+        print("White brightness:", self.current_mode())
+
+
+class RgbModes(AbstractLightingMode):
+    RED = (1,0,0)
+    YELLOW = (1,1,0)
+    GREEN = (0,1,0)
+    BLUE = (0,0,1)
+    CYAN = (0,1,1)
+    MAGENTA = (1,0,1)
+
+    modes = (RED, YELLOW, GREEN, BLUE, CYAN, MAGENTA)
+
+    def activate(self):
+        print("RGB values:", self.current_mode())
+
+
+class UvModes(AbstractLightingMode):
+    STEADY = 60
+    SLOW_STROBE = 1
+    FAST_STROBE = 2
+
+    modes = (STEADY, SLOW_STROBE, FAST_STROBE)
+
+    def activate(self):
+        print("Black Light Frequency (Strobe)", self.current_mode())
+
+
+class Dark(AbstractLightingMode):
+
+    modes = "X"
+
+    def activate(self):
+        print("OFF")
+
+
+class LightingModes(AbstractLightingMode):
+    WHITE = WhiteModes()
+    RGB = RgbModes()
+    UV = UvModes()
+    OFF = Dark()
+
+    modes = (WHITE, RGB, UV, OFF)
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import machine
 import math, utime
 from touch_button import *
+from lighting_modes import *
 
 from ntptime import settime
 
@@ -12,71 +13,6 @@ led = machine.PWM(machine.Pin(2), freq=60)
 led_touch_button = TouchButton(machine.Pin(4), touch_adjust_parameters)
 
 adjust_touch_button = TouchButton(machine.Pin(27), touch_adjust_parameters)
-
-
-class AbstractLightingMode:
-
-    current_mode_index = 0
-
-    def next(self):
-        self.current_mode_index += 1
-        if self.current_mode_index == len(self.modes):
-            self.current_mode_index = 0
-        return self.current_mode()
-
-    def current_mode(self):
-        return self.modes[self.current_mode_index]
-
-
-class WhiteModes(AbstractLightingMode):
-    FULL = 1023
-    HALF = 512
-    QUARTER = 256
-
-    modes = (FULL, HALF, QUARTER)
-
-    def activate(self):
-        print("White brightness:", self.current_mode())
-
-
-class RgbModes(AbstractLightingMode):
-    RED = (1,0,0)
-    YELLOW = (1,1,0)
-    GREEN = (0,1,0)
-    BLUE = (0,0,1)
-    CYAN = (0,1,1)
-    MAGENTA = (1,0,1)
-
-    modes = (RED, YELLOW, GREEN, BLUE, CYAN, MAGENTA)
-
-    def activate(self):
-        print("RGB values:", self.current_mode())
-
-class UvModes(AbstractLightingMode):
-    STEADY = 60
-    SLOW_STROBE = 1
-    FAST_STROBE = 2
-
-    modes = (STEADY, SLOW_STROBE, FAST_STROBE)
-
-    def activate(self):
-        print("Black Light Frequency (Strobe)", self.current_mode())
-
-class Dark(AbstractLightingMode):
-
-    modes = "X"
-
-    def activate(self):
-        print("OFF")
-
-
-class LightingModes(AbstractLightingMode):
-    WHITE = WhiteModes()
-    RGB = RgbModes()
-    UV = UvModes()
-    OFF = Dark()
-
-    modes = (WHITE, RGB, UV, OFF)
 
 
 def demo_modes():
