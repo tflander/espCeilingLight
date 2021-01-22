882cffc1c8cac4d3376a10f48b41bd4cf28d7a46
todd flanders <toddfbass@gmail.com>
2021-01-22 10:11:43 -0500

    define the PWM channels

--- main.py
+++ main.py
@@ -14,10 +14,15 @@ led = machine.PWM(machine.Pin(2), freq=4, duty=512)
 led_touch_button = TouchButton(machine.Pin(4), touch_adjust_parameters)
 
 
-adjust_touch_button = TouchButton(machine.Pin(27), touch_adjust_parameters)
+mode_touch_button = TouchButton(machine.Pin(4), touch_adjust_parameters)
+sub1_touch_button = TouchButton(machine.Pin(27), touch_adjust_parameters)
+sub2_touch_button = TouchButton(machine.Pin(14), touch_adjust_parameters)
 
-# green = machine.PWM(machine.Pin(23), freq=60, duty=512)
-# blue = machine.PWM(machine.Pin(22), freq=60, duty=512)
+red = machine.PWM(machine.Pin(21), freq=60, duty=16)
+green = machine.PWM(machine.Pin(23), freq=60, duty=512)
+blue = machine.PWM(machine.Pin(22), freq=60, duty=16)
+white = machine.PWM(machine.Pin(19), freq=60, duty=16)
+ultra_violet = machine.PWM(machine.Pin(18), freq=60, duty=16)
 
 class LightingModes(AbstractLightingMode):
     WHITE = WhiteModes()
