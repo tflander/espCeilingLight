37d3020e63be720c3818b4456c0bb15fe5440f65
todd flanders <toddfbass@gmail.com>
2021-01-31 18:23:19 -0500

    control lighting on boot, clean up code

--- main.py
+++ main.py
@@ -18,55 +18,75 @@ sub2_touch_button = TouchButton(machine.Pin(14), touch_adjust_parameters)
 led_pwm_channels = LedPwmChannels(red_pin=21, green_pin=23, blue_pin=22, white_pin=19, uv_pin=18)
 
 
-# class LightingModes(AbstractLightingMode):
-#     WHITE = WhiteModes(led_pwm_channels)
-#     RGB = RgbModes(led_pwm_channels)
-#     UV = UvModes(led_pwm_channels)
-#     Party = LightModes(led_pwm_channels)
-#     OFF = Dark()
-#
-#     modes = (WHITE, RGB, UV, Party)  # for now, no OFF mode so I don't burn the house down.
-#
-#
-# lights = LightingModes()
-# lights.current_mode().activate()
-#
-#
-# def light_it_up():
-#     print("running lighting modes...")
-#     while True:
-#         if mode_touch_button.is_state_changed():
-#             if mode_touch_button.state == TouchState.SELECTED:
-#                 lights.current_mode().deactivate()
-#                 lights.next()
-#                 lights.current_mode().activate()
-#
-#         if sub1_touch_button.is_state_changed():
-#             if sub1_touch_button.state == TouchState.SELECTED:
-#                 lights.current_mode().deactivate()
-#                 lights.current_mode().next()
-#                 lights.current_mode().activate()
-#
-#         if sub2_touch_button.is_state_changed():
-#             if sub2_touch_button.state == TouchState.SELECTED:
-#                 lights.current_mode().deactivate()
-#                 lights.current_mode().next_adjustment()
-#                 lights.current_mode().activate()
-#
-#
-# def party_spike():
-#     lights.current_mode().deactivate()
-#
-#     party_modes = lights.modes[3]
-#     party_modes.activate()
-#
-#     while True:
-#         if mode_touch_button.is_state_changed():
-#             if mode_touch_button.state == TouchState.SELECTED:
-#                 party_modes.deactivate()
-#                 party_modes.next()
-#                 party_modes.activate()
+def control_lighting():
+    party_modes = LightModes(led_pwm_channels)
+    led_pwm_channels.zero_duty()
+    button_collection = TouchButtonCollection(mode_touch_button, sub1_touch_button, sub2_touch_button)
+    party_modes.activate()
+
+    while True:
+
+        selected_button = await button_collection.wait_for_button_select()
+
+        party_modes.deactivate()
+        if selected_button == 0:
+            party_modes.next_mode()
+        elif selected_button == 1:
+            party_modes.next_hue()
+        else:
+            party_modes.next_brightness_or_speed()
+        party_modes.activate()
+        uasyncio.sleep_ms(10)
 
 
-# light_it_up()
-# uasyncio.run(party_spike())
+class LightModes:
+
+    def __init__(self, pwm_channels: LedPwmChannels):
+        self.pwm_channels = pwm_channels
+        self.current_intensity_index = 0
+        self.task = None
+        self.current_mode_index = 0
+
+        self.modes = (
+            WhiteModes(self.pwm_channels),
+            MultiColorFlash(self.pwm_channels, (
+                (RgbColors.RED, RgbColors.BLUE),
+                (RgbColors.BLUE, RgbColors.YELLOW),
+                (RgbColors.RED, RgbColors.YELLOW, RgbColors.GREEN, RgbColors.CYAN, RgbColors.BLUE, RgbColors.MAGENTA)
+            )),
+            OneColorGlow(self.pwm_channels, (
+                RgbColors.RED,
+                RgbColors.GREEN,
+                RgbColors.BLUE,
+                RgbColors.CYAN,
+                RgbColors.YELLOW,
+                RgbColors.MAGENTA)
+            )
+        )
+
+    def current_mode(self):
+        return self.modes[self.current_mode_index]
+
+    def activate(self):
+        self.task = uasyncio.create_task(self.current_mode().activate())
+
+    def next_mode(self):
+        self.current_mode_index += 1
+        if self.current_mode_index == len(self.modes):
+            self.current_mode_index = 0
+
+    def next_hue(self):
+        self.current_mode().next_hue()
+
+    def next_brightness_or_speed(self):
+        self.current_mode().next_brightness_or_speed()
+
+    def deactivate(self):
+        if self.task is not None:
+            self.task.cancel()
+            self.task = None
+        led_pwm_channels.zero_duty()
+
+
+print("running lights")
+uasyncio.run(control_lighting())
--- party.py
+++ party.py
@@ -1,39 +1,4 @@
-# TODO: move abstract stuff to a different library
-from lighting_modes import *
 import uasyncio
-from touch_button import *
-
-led_pwm_channels = LedPwmChannels(red_pin=21, green_pin=23, blue_pin=22, white_pin=19, uv_pin=18)
-touch_adjust_parameters = AdjustParameters(limits=(50, 600), dead_band=(175, 250))
-mode_touch_button = TouchButton(machine.Pin(4), touch_adjust_parameters)
-sub1_touch_button = TouchButton(machine.Pin(27), touch_adjust_parameters)
-sub2_touch_button = TouchButton(machine.Pin(14), touch_adjust_parameters)
-
-
-def control_animation():
-    party_modes = LightModes(led_pwm_channels)
-    led_pwm_channels.zero_duty()
-    button_collection = TouchButtonCollection(mode_touch_button, sub1_touch_button, sub2_touch_button)
-    party_modes.activate()
-
-    while True:
-
-        selected_button = await button_collection.wait_for_button_select()
-
-        party_modes.deactivate()
-        if selected_button == 0:
-            party_modes.next_mode()
-        elif selected_button == 1:
-            party_modes.next_hue()
-        else:
-            party_modes.next_brightness_or_speed()
-        party_modes.activate()
-        uasyncio.sleep_ms(10)
-
-
-def doit():
-    uasyncio.run(control_animation())
-
 
 class OneColorGlow:
 
@@ -97,46 +62,5 @@ class MultiColorFlash:
     def current_delay(self):
         return MultiColorFlash.delays[self.delay_index]
 
-
-class LightModes:
-
-    def __init__(self, pwm_channels: LedPwmChannels):
-        self.pwm_channels = pwm_channels
-        self.current_intensity_index = 0
-        self.task = None
-        self.current_mode_index = 0
-
-        self.modes = (
-            WhiteModes(self.pwm_channels),
-            MultiColorFlash(self.pwm_channels, (
-                (RgbColors.RED, RgbColors.BLUE),
-                (RgbColors.BLUE, RgbColors.YELLOW),
-                (RgbColors.RED, RgbColors.YELLOW, RgbColors.GREEN, RgbColors.CYAN, RgbColors.BLUE, RgbColors.MAGENTA)
-            )),
-            OneColorGlow(self.pwm_channels, (RgbColors.RED, RgbColors.GREEN, RgbColors.BLUE, RgbColors.CYAN, RgbColors.YELLOW, RgbColors.MAGENTA))
-        )
-
-    def current_mode(self):
-        return self.modes[self.current_mode_index]
-
-    def activate(self):
-        self.task = uasyncio.create_task(self.current_mode().activate())
-
-    def next_mode(self):
-        self.current_mode_index += 1
-        if self.current_mode_index == len(self.modes):
-            self.current_mode_index = 0
-
-    def next_hue(self):
-        self.current_mode().next_hue()
-
-    def next_brightness_or_speed(self):
-        self.current_mode().next_brightness_or_speed()
-
-    def deactivate(self):
-        if self.task is not None:
-            self.task.cancel()
-            self.task = None
-        led_pwm_channels.zero_duty()
 
 
