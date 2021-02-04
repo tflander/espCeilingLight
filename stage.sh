bbffb7401b96d7f006de5aacfd55a937ec6da848
todd flanders <toddfbass@gmail.com>
2021-02-04 15:20:58 -0500

    created script for staging

---
+++ stage.sh
@@ -0,0 +1,20 @@
+export AMPY_PORT=/dev/cu.SLAB_USBtoUART
+ampy ls
+
+echo "updating boot.py..."
+ampy put boot.py
+echo "updating lighting_modes.py..."
+ampy put lighting_modes.py
+echo "updating lighting_support.py..."
+ampy put lighting_support.py
+echo "updating main.py..."
+ampy put main.py
+echo "updating party.py..."
+ampy put party.py
+echo "updating touch_button.py..."
+ampy put touch_button.py
+echo "updating wifiConnector.py..."
+ampy put wifiConnector.py
+echo "resetting..."
+ampy reset
+echo "done."
