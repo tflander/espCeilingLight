4cf9bae6a85264373f16ff084c90e79b43020094
todd flanders <toddfbass@gmail.com>
2021-03-31 16:54:43 -0400

    use fixture to create touch button for testing

--- tests/test_touch_button.py
+++ tests/test_touch_button.py
@@ -3,28 +3,25 @@ from touch_button import AdjustParameters, TouchButton, TouchState
 import machine
 
 
-def test_touch_button_default_state_is_unknown():
+@pytest.fixture
+def touch_button():
     touch_adjust_parameters = AdjustParameters(limits=(50, 600), dead_band=(175, 250))
-    mode_touch_button = TouchButton(machine.Pin(4), touch_adjust_parameters)
-    assert mode_touch_button.state == TouchState.UNKNOWN
-    # assert not mode_touch_button.is_state_changed()
+    return TouchButton(machine.Pin(4), touch_adjust_parameters)
 
 
-def test_touch_button_set_state_to_selected():
-    touch_adjust_parameters = AdjustParameters(limits=(50, 600), dead_band=(175, 250))
-    mode_touch_button = TouchButton(machine.Pin(4), touch_adjust_parameters)
-
-    mode_touch_button.touch.expect_next_read_value(125)
-
-    assert mode_touch_button.is_state_changed()
-    assert mode_touch_button.state == TouchState.SELECTED
+def test_touch_button_default_state_is_unknown(touch_button):
+    assert touch_button.state == TouchState.UNKNOWN
 
 
-def test_touch_button_set_state_to_released():
-    touch_adjust_parameters = AdjustParameters(limits=(50, 600), dead_band=(175, 250))
-    mode_touch_button = TouchButton(machine.Pin(4), touch_adjust_parameters)
+def test_touch_button_set_state_to_selected(touch_button):
+    touch_button.touch.expect_next_read_value(125)
 
-    mode_touch_button.touch.expect_next_read_value(400)
+    assert touch_button.is_state_changed()
+    assert touch_button.state == TouchState.SELECTED
 
-    assert mode_touch_button.is_state_changed()
-    assert mode_touch_button.state == TouchState.RELEASED
+
+def test_touch_button_set_state_to_released(touch_button):
+    touch_button.touch.expect_next_read_value(400)
+
+    assert touch_button.is_state_changed()
+    assert touch_button.state == TouchState.RELEASED
