10d3a09807f895d2a257ff1b4df68bbdc394ac0c
todd flanders <toddfbass@gmail.com>
2021-04-16 10:50:16 -0400

    calc color slice deltas for fade as float

--- animation_calculator.py
+++ animation_calculator.py
@@ -1,18 +1,33 @@
 from lighting_support import LedPwmChannels
-from rgb_duties_converter import RgbDutiesConverter
+from rgb_duties_converter import RgbDutiesConverter, Duties
 import math
+
+
+class FadeParams():
+
+    def __init__(self, slice_duration_ms, target_color, color_total_deltas, color_slice_deltas):
+        self.color_slice_deltas = color_slice_deltas
+        self.color_total_deltas = color_total_deltas
+        self.target_color = target_color
+        self.slice_duration_ms = slice_duration_ms
 
 
 class AnimationCalculator:
     @staticmethod
-    def for_fade_command(command, led_pwm_channels: LedPwmChannels):
-        slice_duration_ms = 10
+    def for_fade_command(command, led_pwm_channels: LedPwmChannels, slice_duration_ms = 10):
         target_color = RgbDutiesConverter.to_duties(command['color'])
-        color_deltas = led_pwm_channels.delta_to_color(target_color)
+        color_total_deltas = led_pwm_channels.delta_to_color(target_color)
         total_delay_ms = AnimationCalculator.delay_time_ms(command)
         slice_count = math.floor(total_delay_ms / slice_duration_ms)
         x = 0
-        # 3) calculate the value to add or subtract for each color for every fade step
+        r = color_total_deltas.red / slice_count
+        g = color_total_deltas.green / slice_count
+        b = color_total_deltas.blue / slice_count
+        w = color_total_deltas.white / slice_count
+        u = color_total_deltas.ultra_violet / slice_count
+        color_slice_deltas = Duties(r, g, b, w, u)
+
+        return FadeParams(slice_duration_ms, target_color, color_total_deltas, color_slice_deltas)
 
     @staticmethod
     def delay_time_ms(command):
--- tests/test_animation_calculator.py
+++ tests/test_animation_calculator.py
@@ -1,14 +1,51 @@
 from lighting_support import LedPwmChannels
 from animation_calculator import AnimationCalculator
+import pytest
+
+from rgb_duties_converter import Duties
 
 
-def test_foo():
-    fade_command = {"command": "fade", "time": 1, "unit": "s", "color": "#ffff00"}
-    pwm_channels = LedPwmChannels(red_pin=2, green_pin=3, blue_pin=4, white_pin=5, uv_pin=6)
+@pytest.fixture
+def pwm_channels():
+    pwm_channels = LedPwmChannels(red_pin=1, green_pin=2, blue_pin=3, white_pin=4, uv_pin=5)
     pwm_channels.red.duty(10)
     pwm_channels.green.duty(20)
     pwm_channels.blue.duty(30)
     pwm_channels.white.duty(40)
     pwm_channels.ultra_violet.duty(50)
+    return pwm_channels
 
-    AnimationCalculator.for_fade_command(fade_command, pwm_channels)
+
+def test_fade_time_slice_defaults_to_10ms(pwm_channels):
+    fade_command = {"command": "fade", "time": 1, "unit": "s", "color": "#ffff00"}
+
+    fade_params = AnimationCalculator.for_fade_command(fade_command, pwm_channels)
+    assert fade_params.slice_duration_ms == 10
+
+
+def test_can_override_default_fade_time_slice(pwm_channels):
+    fade_command = {"command": "fade", "time": 1, "unit": "s", "color": "#ffff00"}
+
+    fade_params = AnimationCalculator.for_fade_command(fade_command, pwm_channels, slice_duration_ms=20)
+    assert fade_params.slice_duration_ms == 20
+
+
+def test_fade_target_color(pwm_channels):
+    fade_command = {"command": "fade", "time": 1, "unit": "s", "color": "#ffcc00"}
+
+    fade_params = AnimationCalculator.for_fade_command(fade_command, pwm_channels)
+    assert fade_params.target_color == Duties(red=1020, green=816)
+
+
+def test_fade_color_total_deltas(pwm_channels):
+    fade_command = {"command": "fade", "time": 1, "unit": "s", "color": "#ffcc00"}
+
+    fade_params = AnimationCalculator.for_fade_command(fade_command, pwm_channels)
+    assert fade_params.color_total_deltas == Duties(red=1010, green=796, blue=-30, white=-40, ultra_violet=-50)
+
+
+def test_fade_color_slice_deltas(pwm_channels):
+    fade_command = {"command": "fade", "time": 1, "unit": "s", "color": "#ffcc00"}
+
+    fade_params = AnimationCalculator.for_fade_command(fade_command, pwm_channels, slice_duration_ms=10)
+    assert fade_params.color_slice_deltas == Duties(red=10.1, green=7.96, blue=-0.3, white=-0.4, ultra_violet=-0.5)
