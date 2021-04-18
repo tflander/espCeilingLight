cd549dc323eb5da63105757cc0e6eae0624a760e
todd flanders <toddfbass@gmail.com>
2021-04-18 15:31:25 -0400

    update stage script

--- stage.sh
+++ stage.sh
@@ -21,9 +21,11 @@ echo "updating rgb_duties_converter.py..."
 ampy put rgb_duties_converter.py
 echo "updating lighting_script_runner.py..."
 ampy put lighting_script_runner.py
+echo "updating animation_calculator.py..."
+ampy put animation_calculator.py
 
 echo "updating web_route_controllers.py..."
-ampy rmdir web_control
+ampy rmdir web_control || echo "ignoring that folder web_control does not exist"
 ampy mkdir web_control
 ampy put web_control/web_route_controllers.py web_control/web_route_controllers.py
 echo "updating lighting_request.py..."
