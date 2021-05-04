c9afa303350e4e527fd65b498a5cc87a33139cf3
todd flanders <toddfbass@gmail.com>
2021-05-04 14:26:24 -0400

    add let example that uses an existing script var

--- tests/dsl_spike/test_dsl.py
+++ tests/dsl_spike/test_dsl.py
@@ -6,30 +6,12 @@ def test_variable_assignment():
     script_runner = ScriptRunner()
     script = [
         "let y = add(2, 1)",
+        "let x = y + 1",
         "show y"
     ]
 
-    for line in script:
-        parts = line.split()
-        if parts[0] == 'let':
-            print("variable assignment", parts)
-
-            let_pattern = r"^let\s+(.*)=(.*)$"
-            p = re.compile(let_pattern)
-            m = p.match(line)
-            print(m)
-            if m is None:
-                print('invalid syntax', line, 'for', let_pattern)
-            script_var = m.group(1).strip()
-            rhs = m.group(2).strip()
-
-            script_runner.let(script_var, rhs)
-            print(script_runner.script_vars)
-            continue
-        if parts[0] == 'show':
-            script_runner.show(parts[1])
-            continue
-        print('syntax error', line)
+    script_runner.run(script)
+    assert script_runner.script_vars == {'y': 3, 'x': 4}
 
 
 class ScriptRunner:
@@ -37,7 +19,28 @@ class ScriptRunner:
     def __init__(self):
         self.script_vars = {}
 
+    def run(self, script):
+        for line in script:
+            parts = line.split()
+            if parts[0] == 'let':
+                let_pattern = r"^let\s+(.*)=(.*)$"
+                p = re.compile(let_pattern)
+                m = p.match(line)
+                if m is None:
+                    print('invalid syntax', line, 'for', let_pattern)
+                script_var = m.group(1).strip()
+                rhs = m.group(2).strip()
+
+                self.let(script_var, rhs)
+                continue
+            if parts[0] == 'show':
+                self.show(parts[1])
+                continue
+            print('syntax error', line)
+
     def let(self, k, v):
+        # TODO: resolve script variables in v
+        print(v)
         self.script_vars[k] = eval(v)
 
     def show(self, script_var):
