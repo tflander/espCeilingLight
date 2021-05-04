5074d5af996b84c9a0fc2ef882eb15a691b785d3
todd flanders <toddfbass@gmail.com>
2021-05-04 10:46:51 -0400

    create script runner

--- tests/dsl_spike/test_dsl.py
+++ tests/dsl_spike/test_dsl.py
@@ -1,22 +1,7 @@
-
-def test_eval():
-    x = 1
-    script = [
-        "print(x)",
-        "print(add(x, 1))"
-    ]
-
-    for line in script:
-        eval(line)
-
-
-script_vars = {}
-
-
 def test_variable_assignment():
-    global script_vars
+    script_runner = ScriptRunner()
     script = [
-        "let y = add(x, 1)",
+        "let y = add(2, 1)",
         "show y"
     ]
 
@@ -24,19 +9,28 @@ def test_variable_assignment():
         parts = line.split()
         if parts[0] == 'let':
             print("variable assignment")
-            script_vars['y'] = eval('add(3, 1)')
-            print(script_vars)
+
+            script_runner.let('y', script_runner.add(3, 1))
+
+            print(script_runner.script_vars)
             continue
         if parts[0] == 'show':
-            show(parts[1])
+            script_runner.show(parts[1])
             continue
-        eval(line)
+        print('syntax error', line)
 
 
-def add(v1, v2):
-    return v1 + v2
+class ScriptRunner:
 
+    def __init__(self):
+        self.script_vars = {}
 
-def show(script_var):
-    global script_vars
-    print(script_vars[script_var])
+    def let(self, k, v):
+        self.script_vars[k] = v
+
+    def add(self, v1, v2):
+        return v1 + v2
+
+    def show(self, script_var):
+        print(self.script_vars[script_var])
+
