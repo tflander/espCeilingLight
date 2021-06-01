c699610b9a303f23afce706080586b3f485bf737
todd flanders <toddfbass@gmail.com>
2021-06-01 16:31:23 -0400

    dead code cleanup

--- parsers/lighting_script_parser.py
+++
@@ -1,9 +0,0 @@
-from parsers.lighting_command_node import LightingCommandNode
-from parsers.parser_constants import LightingCommandNodeTypes
-
-
-class LightingScriptParser:
-    def parse(self, candidate_command):
-        # TODO: for now, assume expression
-        return LightingCommandNode(LightingCommandNodeTypes.EXPR, candidate_command)
-
