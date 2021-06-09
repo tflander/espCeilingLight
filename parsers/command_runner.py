2cbae7caaf971fd8b705178f7561ed7babaecc77
todd flanders <toddfbass@gmail.com>
2021-06-09 12:31:01 -0400

    start test driving operator resolution

--- parsers/command_runner.py
+++ parsers/command_runner.py
@@ -1,4 +1,5 @@
 from parsers.command_parser import parse_command
+from parsers.parser_constants import ExpressionValueTypes
 
 
 def run_command(commands):
@@ -18,11 +19,40 @@ class CommandScope:
 
         self.is_parsed = True
 
-    # local_variables = {}
-    # command_pointer = 0
-    #
-    # def step_command(self):
-    #     pass
-    #
-    # def value_for_local(self, variable_name):
-    #     pass
+        self.local_variables = {}
+        self.command_pointer = 0
+
+    def step_command(self):
+        command = self.parse_results[self.command_pointer]
+        var_name = command.left.match
+        if command.right.value is not None:
+            value = command.right.value
+        else:
+            value = self.resolve_expression(command.right)
+        self.local_variables[var_name] = value
+
+        self.command_pointer += 1
+
+    def value_for_local(self, variable_name):
+        if variable_name not in self.local_variables:
+            return None
+        return self.local_variables[variable_name]
+
+    def resolve_expression(self, result):
+        operator = result.match
+        left_operand = self.resolve_operand(result.left)
+        right_operand = self.resolve_operand(result.right)
+        return self.resolve_operator(operator, left_operand, right_operand)
+
+    def resolve_operand(self, result):
+        if result.value is not None:
+            return result.value
+        else:
+            return self.local_variables[result.match]
+
+    @staticmethod
+    def resolve_operator(operator, left_operand, right_operand):
+        if operator == ExpressionValueTypes.ADDITION:
+            return left_operand + right_operand
+        elif operator == ExpressionValueTypes.MULTIPLICATION:
+            return left_operand * right_operand
--- tests/parsers/test_command_runner.py
+++ tests/parsers/test_command_runner.py
@@ -13,3 +13,27 @@ def test_script_parse():
     assert run_scope.is_parsed
     assert that(run_scope.parse_results[0]).is_assignment("x", 0)
     assert that(run_scope.parse_results[1]).is_assignment("x", "x+1")
+
+# TODO: degenerate test parse for invalid script
+
+
+def test_script_operations():
+    commands = [
+        "x = 0",
+        "x = x+1",
+        "x = x * 5"
+    ]
+    # TODO: test all operations
+
+    run_scope = CommandScope(commands)
+    assert run_scope.value_for_local("x") is None
+    run_scope.step_command()
+    assert run_scope.value_for_local("x") == 0
+    run_scope.step_command()
+    assert run_scope.value_for_local("x") == 1
+    run_scope.step_command()
+    assert run_scope.value_for_local("x") == 5
+
+
+# TODO: degenerate test run for undefined variable
+# TODO: degenerate test for running past end of script?
