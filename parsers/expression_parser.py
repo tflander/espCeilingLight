cda61990bd3b16c7443e4e7308e155aa0a58319c
todd flanders <toddfbass@gmail.com>
2021-06-15 10:17:01 -0400

    move combine operator results to operator parser

--- parsers/expression_parser.py
+++ parsers/expression_parser.py
@@ -136,37 +136,6 @@ def combine_expression_results(results):
     return combine_results(results, expression_combinators)
 
 
-def combine_operator_results(results, operator_value_type, value_combiner):
-    for i, result in enumerate(results):
-        if result.result_type == operator_value_type:
-            left_operand = results[i-1]
-            right_operand = results[i+1]
-            token = left_operand.match + results[i].match + right_operand.match
-            combined_result = ParseResult(token, operator_value_type, ExpressionValueTypes.OPERATION)
-
-            if not is_valid_operand(left_operand) or not is_valid_operand(right_operand):
-                failure = CombineFailure(token)
-                if not is_valid_operand(left_operand):
-                    failure.message = "invalid left operand: " + left_operand.match
-                else:
-                    failure.message = "invalid right operand: " + right_operand.match
-                return failure, False
-
-            if left_operand.value is not None and right_operand.value is not None:
-                combined_result.value = value_combiner(left_operand.value, right_operand.value)
-            else:
-                combined_result.left = left_operand
-                combined_result.right = right_operand
-            return results[:i-1] + [combined_result] + results[i+2:], True
-    return results, False
-
-
-def is_valid_operand(result):
-    return result.value is not None \
-        or result.result_type == ExpressionValueTypes.VARIABLE \
-        or result.result_type == ExpressionValueTypes.OPERATION
-
-
 def consume_comma(token):
     return parse_generic(token, comma_pattern, ExpressionValueTypes.COMMA)
 
--- parsers/operator_parser.py
+++ parsers/operator_parser.py
@@ -46,3 +46,35 @@ def parse_left_paren(token):
 
 def parse_right_paren(token):
     return parse_generic(token, right_paren_pattern, ExpressionValueTypes.RIGHT_PAREN)
+
+
+def is_valid_operand(result):
+    return result.value is not None \
+        or result.result_type == ExpressionValueTypes.VARIABLE \
+        or result.result_type == ExpressionValueTypes.OPERATION
+
+
+def combine_operator_results(results, operator_value_type, value_combiner):
+    for i, result in enumerate(results):
+        if result.result_type == operator_value_type:
+            left_operand = results[i-1]
+            right_operand = results[i+1]
+            token = left_operand.match + results[i].match + right_operand.match
+            combined_result = ParseResult(token, operator_value_type, ExpressionValueTypes.OPERATION)
+
+            if not is_valid_operand(left_operand) or not is_valid_operand(right_operand):
+                failure = CombineFailure(token)
+                if not is_valid_operand(left_operand):
+                    failure.message = "invalid left operand: " + left_operand.match
+                else:
+                    failure.message = "invalid right operand: " + right_operand.match
+                return failure, False
+
+            if left_operand.value is not None and right_operand.value is not None:
+                combined_result.value = value_combiner(left_operand.value, right_operand.value)
+            else:
+                combined_result.left = left_operand
+                combined_result.right = right_operand
+            return results[:i-1] + [combined_result] + results[i+2:], True
+    return results, False
+
